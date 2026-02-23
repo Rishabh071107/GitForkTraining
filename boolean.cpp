d537fdf24acf7abf85f38e19d7e628851753f5ea
Rishabh071107 <rishabh71107@gmail.com>
2026-02-23 11:14:02 +0530

    file created

---
+++ boolean.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+void mergeArrays(int arr1[], int size1, int arr2[], int size2, int merged[]) {
+   
+    for (int i = 0; i < size1; i++) {
+        merged[i] = arr1[i];
+    }
+    
+    
+    for (int i = 0; i < size2; i++) {
+        merged[size1 + i] = arr2[i];
+    }
+}
+
+
+void printArray(int arr[], int size) {
+    cout << "Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    int arr1[] = {1, 2, 3, 4, 5};
+    int size1 = 5;
+    
+    
+    int arr2[] = {6, 7, 8, 9, 10};
+    int size2 = 5;
+    
+    
+    int merged[size1 + size2];
+    
+
+    mergeArrays(arr1, size1, arr2, size2, merged);
+    
+            
+    cout << "First Array: ";
+    printArray(arr1, size1);
+    
+    cout << "Second Array: ";
+    printArray(arr2, size2);
+    
+    cout << "Merged Array: ";
+    printArray(merged, size1 + size2);
+    
+    return 0;
+}
